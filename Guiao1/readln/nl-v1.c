78c4a55481e5509a66c96601105b4121336ff32e
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2019-02-22 15:53:10 +0000

    fix bugs

--- Guiao1/readln/nl-v1.c
+++ Guiao1/readln/nl-v1.c
@@ -1,18 +1,18 @@
 #include "readln.h"
 
 int main(int argc, char ** argv){
-    int readC, line = 1;
-    int size = 10;
+    int readC, f, line = 1;
+    int size = 10000;
     char * buf = malloc(size);
 
-    int f = (argc == 1) ? 0 : open(argv[1], O_RDONLY);
+    if (argc == 1){ f = 0; }
+    else{f = open(argv[1], O_RDONLY); }
 
     do{
         readC = readln (f, buf, size);
         if (readC == 0) break;
         if (readC > 1){
-            if(buf[readC] == '\n')
-                dprintf(1, "\t%d ", line);
+            dprintf(1, "\t%d ", line);
             write (1, buf, readC);
             line++;
         }
