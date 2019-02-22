e8e29ee55fd1ccaa09ac2371ed9e1e26b45d61ff
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2019-02-22 15:14:17 +0000

    resolução da aula de readln

--- Guiao1/readln/readln.c
+++ Guiao1/readln/readln.c
@@ -7,9 +7,8 @@ ssize_t readln(int fildes, void *buf, size_t nbyte){
     char * buff = (char *) buf;
 
     while(c != '\n' && i < nbyte){
-
        readC = read(fildes, &c, 1);
-       if (readC == 0) return 0;
+       if (readC <= 0) return 0;
        buff[i] = c;
        i ++;
     }
@@ -17,16 +16,16 @@ ssize_t readln(int fildes, void *buf, size_t nbyte){
     return (i);
 }
 
-/*
-int main(int argc, char ** argv){
-    int rLen;
-    int size = atoi(argv[1]);
-    char * buf = malloc(size);
-    do{
-        rLen = readln (0, buf, size);
-        write (1, buf, rLen);
-    }while(rLen);
-    
-    return 0;
+/*RESOLUÇÂO DA AULA
+
+ssize_t readln(int fildes, void *buf, size_t nbyte){
+    ssize_t n, i = 0;
+    char * b = buf;
+    while(i < nb){
+        n = read(fd, b + i, 1)
+        if (n <= 0) break;
+        if (b[i++] == '\n') break;
+    }
+    return i;
 }
 */
