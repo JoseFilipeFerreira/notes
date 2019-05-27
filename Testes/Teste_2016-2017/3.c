3d70bf377c0bee32219455fddae132bdbec5f52f
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2019-05-27 18:07:22 +0100

    If it doesn't compile it surelly doesen't work

--- Testes/Teste_2016-2017/3.c
+++ Testes/Teste_2016-2017/3.c
@@ -3,10 +3,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-
+#include <string.h>
 
 #define SIZE 4096
-
 
 ssize_t readln(int fildes, void *buff, size_t nbyte) {
     size_t i;
@@ -16,35 +15,60 @@ ssize_t readln(int fildes, void *buff, size_t nbyte) {
     return i;
 }
 
-int main(int argc, char** argv){
+void filterExist(int fd){
+    int pipefd[2];
+    pipe(pipefd);
+    dup2(pipefd[1], 1);
+    close(pipefd[1]);
 
-    int pipefd[argc - 1][2];
-
-    for(int i = 0; i < argc - 1; i++){
-        pipe(pipefd [i]);
-        if(!fork()){
-            dup2(pipefd[i][1], 1);
-            close(pipefd[i][0]);
-            close(pipefd[i][1]);
-            execlp(argv[i + 1], argv[i + 1], NULL);
-            exit(1);
-        }
-        close(pipefd[i][1]);
+    dup2(fd, 0);
+    if(!fork()){
+        execlp("filtro", "filtro", NULL);
+        exit(0);
     }
-
-    int nRead, exists = 1;
-    char buff[SIZE];
-    while(exists){
-        exists = 0;
-        for(int i = 0; i < argc - 1; i++)
-            for(int j = 0; j < 10; j++)
-                if(nRead = readln(pipefd[i][0], buff, SIZE))
-                    exists += write(1, buff, nRead);
+    dup2(pipefd[0], 0);
+    close(pipefd[0]);
+    int pid = fork();
+    if(!pid){
+        execlp("existe", "existe", NULL);
+        exit(1);
     }
-
-    return 0;
+    int status;
+    waitpid(pid, &status, WUNTRACED);
+    exit((WIFEXITED(status))? WEXITSTATUS(status) : 0);
 }
 
+int main(int argc, char** argv){
+    int nThreads = atoi(argv[1]);
+    int pipefd[nThreads][2];
+    for(int i = 0; i < atoi(argv[1]); i++){
+        pipe(pipefd[i]);
+        if(!fork())
+            filterExist(pipefd[i][0]);
+    }
 
+    char buff[SIZE];
+    int nRead, exists = 1;
+    while(exists){
+        exists = 0;
+        for(int i = 0; i < nThreads; i++){
+            nRead = readln(0, buff, SIZE);
+            exists += write(pipefd[i][1], buff, nRead);
+        }
+    }
 
+    int status;
+    for(int i = 0; i < nThreads; i++){
+        wait(&status);
+        if(WIFEXITED(status)){
+            if(WEXITSTATUS(status)){
+                write(0, "1", strlen("1"));
+                return 0;
+            }
+        }
+    }
+
+    write(0, "0", strlen("0"));
+    return 0;
+}
 
