278ad4a6fb3a3f033ede3ad4ece153d12e25ea11
JoseFilipeFerreira <36403066+JoseFilipeFerreira@users.noreply.github.com>
2022-06-07 00:32:42 +0100

    update asp

--- AP/asp/main.c
+++ AP/asp/main.c
@@ -1,14 +1,19 @@
+#include <omp.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-#define N 100
+#define N 2000
+#define MAXD 42
 
-int tab[N][N];
+int tab[N][N]__attribute__((aligned (32)));
 
-void do_asp() {
-    for (int k = 0; k < N; k++) {
-        for (int i = 0; i < N; i++) {
+void asp() {
+    for (int k = 0; k < N; ++k) {
+        for (int i = 0; i < N; ++i) {
             if (i != k) {
-                for (int j = 0; j < N; j++) {
+                for (int j = 0; j < N; ++j) {
                     int tmp = tab[i][k] + tab[k][j];
                     if (tmp < tab[i][j]) {
                         tab[i][j] = tmp;
@@ -17,4 +22,80 @@ void do_asp() {
             }
         }
     }
+}
+
+void asp_parallel_k() {
+#pragma omp parallel for
+    for (int k = 0; k < N; ++k) {
+        for (int i = 0; i < N; ++i) {
+            if (i != k) {
+                for (int j = 0; j < N; ++j) {
+                    int tmp = tab[i][k] + tab[k][j];
+                    if (tmp < tab[i][j]) {
+                        tab[i][j] = tmp;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void asp_parallel_i() {
+    for (int k = 0; k < N; ++k) {
+#pragma omp parallel for
+        for (int i = 0; i < N; ++i) {
+            if (i != k) {
+                for (int j = 0; j < N; ++j) {
+                    int tmp = tab[i][k] + tab[k][j];
+                    if (tmp < tab[i][j]) {
+                        tab[i][j] = tmp;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void asp_swap_parallel_i() {
+#pragma omp parallel for
+    for (int i = 0; i < N; ++i) {
+        for (int k = 0; k < N; ++k) {
+            if (i != k) {
+                for (int j = 0; j < N; ++j) {
+                    int tmp = tab[i][k] + tab[k][j];
+                    if (tmp < tab[i][j]) {
+                        tab[i][j] = tmp;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void test(const char* func_name, void f()) {
+    // populate matrix with random numbers
+    for (size_t i = 0; i < N - 1; i++) {
+        for (size_t j = 0; j < N - 1; j++) {
+            if (j == i)
+                tab[i][j] = 0;
+            else
+                tab[i][j] = 1 + rand() % MAXD;
+        }
+    }
+
+    double time = omp_get_wtime();
+    f();
+    printf("%s: %fs\n", func_name, omp_get_wtime() - time);
+}
+
+int main(void) {
+    time_t t;
+    srand((unsigned) time(&t));
+
+    /* test("asp", asp); */
+    /* test("asp_parallel_k", asp_parallel_k); */
+    /* test("asp_parallel_i", asp_parallel_i); */
+    test("asp_swap_parallel_i", asp_swap_parallel_i);
+
+    return 0;
 }
